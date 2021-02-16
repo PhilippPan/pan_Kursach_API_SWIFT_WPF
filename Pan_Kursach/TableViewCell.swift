a8955563d098bd1146f9d87c552094fb46c61262
Ksenia <ksyuha.kart@mail.ru>
2021-02-16 18:00:48 +0300

    Initial Commit

---
+++ Pan_Kursach/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Pan_Kursach
+//
+//  Created by WSR on 16.02.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userRoleLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var userStatusLabel: UILabel!
+
+}
